39b0d3eff4d92450adf9ae9abbea99a3db0a3e0b
Роман Артемов <rm.artemov@tensor.ru>
2019-03-10 12:00:00 +0300

    add waiting settings

--- ARNotificationTest/Code/ARNotificationSettings.swift
+++ ARNotificationTest/Code/ARNotificationSettings.swift
@@ -15,7 +15,7 @@ struct ARNotificationSettings {
 	var minimumHeight: CGFloat = 66.0
 	
 	/// Минимальный отступ
-	var minimumPadding: CGFloat = 120
+	var minimumPadding: CGFloat = 80
 	
 	/// Минимальный отступ от краев
 	var minimumLRPadding: CGFloat = 8.0
@@ -23,8 +23,11 @@ struct ARNotificationSettings {
 	/// Время исполнения анимации(в одну сторону)
 	var duration: Double = 0.5
 	
+	/// Время ожидания
+	var waiting: Double = 2.0
+	
 	/// Позиция, откуда будут исходить уведомления
-	var position: ARNotificationPosition = .bottom
+	var position: ARNotificationPosition = .top
 	
 	/// Цвета в соотношении с типом уведомлений
 	var colors: [ARNotificationType: UIColor] = [.info: .blue,
